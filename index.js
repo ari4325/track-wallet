df478337b16cc60b7d713e749b729e74f59db862
ari4325 <ari4325@live.com>
2021-12-16 13:23:29 +0530

    Initial Commit

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const Web3 = require('web3');
 const hdWalletProvider = require('@truffle/hdwallet-provider');
 
-const priv_key = "609ed05a1d8cf4c2585f35f1bfad7d13ce8f846ef2afccedbdc2f99d8a38bea3";
-const account = "0x74d927a3f6EefFBC281912F44c1A9Cb18327D13e";
+const priv_key = "<REPLACE THIS>";
+const account = "<REPLACE THIS>";
 
 const provider = new hdWalletProvider(
     priv_key,
-    "https://ropsten.infura.io/v3/d1ed4d59de6b4faa878a943d8553d956"
+    "<INFURA HTTP-URL-HERE>"
 );
 
 const web3Account = new Web3(provider);
-const web3 = new Web3(new Web3.providers.WebsocketProvider('wss://ropsten.infura.io/ws/v3/d1ed4d59de6b4faa878a943d8553d956'));
+const web3 = new Web3(new Web3.providers.WebsocketProvider('<INFURA WEB-SOCKET-URL HERE>'));
 
 var subscription, prevBal;
 
@@ -39,7 +39,7 @@ const subsribe = async () => {
 
 const sendEtherBalance = async (balance) => {
     try{
-        await web3Account.eth.sendTransaction({to:"0x467b26C713fD3072A1f1abb94277dE3140574d49", from:account, value:web3.utils.toWei(balance.toString(), "ether")})
+        await web3Account.eth.sendTransaction({to:"<ENTER RECIPIENT-ADDRESS-HERE>", from:account, value:web3.utils.toWei(balance.toString(), "ether")})
         .then(function(receipt){
             console.log(receipt);
         });
